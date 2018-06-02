04770d8b35b15d38f9fd728ecd95e73475791bfe
Pankaj <mr.pankaj.lilhare@gmail.com>
2018-06-02 15:14:08 -0500

    Test flile

---
+++ JavaPoc/src/com/mypoc/TestPOC.java
@@ -0,0 +1,21 @@
+package com.mypoc;
+class A{
+	A() {
+		System.out.println("--A--");
+	}
+	
+}
+class  B extends A {
+	B() {
+		System.out.println("--B--");
+	}
+}
+public class TestPOC {
+
+	public static void main(String[] args) {
+		B b = new B();
+		
+
+	}
+
+}
